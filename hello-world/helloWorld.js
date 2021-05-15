d7509b47f57f4d50efdce6b09ec664c2e8c6dce6
spencerwagner0@gmail.com <71741236+vvagnersp@users.noreply.github.com>
2021-05-15 13:07:48 -0700

    hello world

--- hello-world/helloWorld.js
+++ hello-world/helloWorld.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function(req,res){
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello world!');
-}).listen(3000);
+}).listen(7431);
 
-console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+console.log('Server started on localhost:7431; press Ctrl-C to terminate....');
