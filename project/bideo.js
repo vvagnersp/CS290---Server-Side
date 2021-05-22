2720d380da06d977d5eb687f5675f81d0412ae1e
spencerwagner0@gmail.com <71741236+vvagnersp@users.noreply.github.com>
2021-05-21 18:31:02 -0700

    Continued project work, store page completed, purchase mostly complete, about mostly complete

--- project/bideo.js
+++ project/bideo.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var hbs = require('express-handlebars').create({defaultLayout:'main', extname: 'hbs'});
+var hbs = require('express-handlebars').create({defaultLayout:'main', extname: 'hbs',});
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
@@ -15,7 +15,68 @@ app.get('/', function(req, res) {
 })
 
 app.get('/store', function(req, res) {
-  res.render('store', {title: 'Store', condition: false});
+  var storeItems = [    
+  {
+    "ID": "001",
+    "name": "Big VHS",
+    "image": "/images/store/giant-4690691_1920.jpg",
+    "rating": "7/10",
+    "description": "Big VHS in the woods, no delivery",
+    "price": "65 USD",
+    "source": "https://pixabay.com/photos/giant-large-huge-vhs-composing-4690691/"
+  },
+
+  {
+    "ID": "002",
+    "name": "Some Equipment",
+    "image": "/images/store/record-4871720_1920.jpg",
+    "rating": "4/10",
+    "description": "A VCR or something and an audio controller (?)",
+    "price": "853,471 yen",
+    "source": "https://pixabay.com/photos/record-player-vintage-technology-4871720/"
+  },
+
+  {
+    "ID": "003",
+    "name": "Broken Cassette",
+    "image": "images/store/cassette-994272_1920.jpg",
+    "rating": "1/10",
+    "description": "Cassette, broken",
+    "price": "1 peso",
+    "source": "https://pixabay.com/photos/cassette-obsolete-chaos-audio-994272/"
+  },
+
+  {
+    "ID": "004",
+    "name": "CD With Hole Drilled in it and a Lock",
+    "image": "images/store/privacy-policy-445153_1920.jpg",
+    "rating": "10/10",
+    "description": "This CD has a hole in it",
+    "price": "400 Euros",
+    "source": "https://pixabay.com/photos/privacy-policy-data-security-445153/"
+  },
+
+  {
+    "ID": "005",
+    "name": "RARE: COMPUTER",
+    "image": "images/store/computer-1895383_1920.jpg",
+    "rating": "3.3241/10",
+    "description": "O l d comp ut er",
+    "price": "13,423 Rubles",
+    "source": "https://pixabay.com/photos/computer-retro-old-privacy-policy-1895383/"
+  },
+
+  {
+    "ID": "006",
+    "name": "Four Floppy Discs",
+    "image": "images/store/black-18320_1920.jpg",
+    "rating": "14/10",
+    "description": "Floppy discs. What secrets do they hold? Good luck reading them.",
+    "price": "4,000,000 AUD",
+    "source": "https://pixabay.com/photos/black-business-computer-computing-18320/"
+  }
+]; 
+  res.render('store', {title: 'Store', items: storeItems, condition: false});
 })
 
 app.get('/purchase', function(req, res) {
